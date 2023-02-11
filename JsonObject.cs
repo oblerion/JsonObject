25af16d1ddbf91684684f65e647b52051964fbec
oblerion <82583559+oblerion@users.noreply.github.com>
2023-02-11 18:07:42 +0100

    Add files via upload

--- JsonObject.cs
+++ JsonObject.cs
@@ -3,46 +3,48 @@ using System.IO;
 using System.Collections.Generic;
 public class JsonObject
 {
-	private Dictionary<string,string> _da = new Dictionary<string,string>(); 
-	public int Size{get=>_da.Count;}
+	private Dictionary<string,string> _ds = new Dictionary<string,string>(); 
+	private Dictionary<string,int> _di = new Dictionary<string, int>();
+	private Dictionary<string,float> _df = new Dictionary<string, float>();
+	private Dictionary<string,JsonObject> _dj = new Dictionary<string,JsonObject>();
+
 	public JsonObject(string sfile)
 	{
-		bool in_obj = false;
-		bool in_array = false;
+		int in_obj = 0;
+		int in_array = 0;
 		List<string> ldata = new List<string>();
 		int i_tps=0;
-		StreamReader file = File.OpenText(sfile);
-        string data = file.ReadToEnd();
+		string data;
+		data = _readFile(sfile);
+		if(data.Length==0) data=sfile;
 		data = _filter(data);
 		data = _extract(data,'{','}');
 		Console.WriteLine($"{data}");
 		for(int i=0;i<data.Length;i++)
 		{
+			switch(data[i])
+			{
+				case '[': in_array++;
+				break;
+				case '{': in_obj++;
+				break;
+				case ']': in_array--;
+				break;
+				case '}': in_obj--;
+				break;
+				case ',':
+					if(in_array<=0 && in_obj<=0)
+					{
+						ldata.Add(data.Substring(i_tps,i-i_tps));
+						i_tps = i+1;
+					}
+				break;
+			}
 			if(i==data.Length-1)
 			{
 				ldata.Add(data.Substring(i_tps,i-i_tps+1));
 			}
-			else if(data[i]=='[')
-			{
-				in_array=true;
-			}
-			else if(data[i]=='{')
-			{
-				in_obj=true;
-			}
-			else if(data[i]==']')
-			{
-				in_array=false;
-			}
-			else if(data[i]=='}')
-			{
-				in_obj=false;
-			}		
-			else if(data[i]==',' && !in_array && !in_obj)
-			{
-				ldata.Add(data.Substring(i_tps,i-i_tps));
-				i_tps = i+1;
-			}
+			
 		}
 		foreach(string s2 in ldata)
 		{
@@ -50,13 +52,50 @@ public class JsonObject
 		 	string value = _extract(s2,':');
 			if(value!="")
 			{
-				if(value[0]=='"') value = _extract(value,'"','"');
+				if(value[0]=='"') _addString(name,value);
+				else if(value[0]=='{')
+				{
+					_dj[name] = new JsonObject(value);
+				}
+				else if(value[0]=='[')
+				{
+
+				}
+				else
+				{
+					_addInt(name,value);
+					_addFloat(name,value);
+				}
 			}
-		 	_da[name]=value;
 			Console.WriteLine($"{name} {value}...");
 		}
 	}
 
+	private void _addString(string name,string value)
+	{
+		_ds[name] =	_extract(value,'"','"');
+	}
+	private void _addInt(string name,string value)
+	{
+		if(!value.Contains('.')) _di[name] = Int32.Parse(value);
+	}
+	private void _addFloat(string name,string value)
+	{
+		if(value!=null && value.Contains('.'))
+		{
+			for(int i=0;i<value.Length;i++)
+			{
+				if(value.ElementAt(i)=='.')
+				{
+					double o = (float)Int32.Parse(value.Substring(0,i));
+					string ls = value.Substring(i+1,value.Length-(i+1));
+					o += Int32.Parse(ls)/Math.Pow(10,ls.Length);
+					_df[name] = (float)o;
+					break;
+				}
+			} 
+		}
+	}
 	private bool IsNumeric(string s)
     {
         foreach (char c in s)
@@ -68,6 +107,19 @@ public class JsonObject
         }
         return true;
     }
+	private string _readFile(string sfile)
+	{
+		
+		if(sfile.Length>0 && File.Exists(sfile))
+		{
+			StreamReader file = File.OpenText(sfile);
+			if(file!=null)
+        		return file.ReadToEnd();
+		}
+		return "";
+	}
+
+
 	private string _filter(string s)
 	{
 		string ls="";
@@ -126,48 +178,53 @@ public class JsonObject
 			return s.Substring(i_beg,s.Length-i_beg);
 		return "";
 	}
+	public override string ToString()
+	{
+		string s="{\n";
+		foreach (var (name,value) in _ds)
+		{
+			s = String.Concat(s,$"\t[{name}] {value}\n");
+		}
+		foreach (var (name,value) in _di)
+		{
+			s = String.Concat(s,$"\t[{name}] {value}\n");
+		}
+		foreach (var (name,value) in _df)
+		{
+			s = String.Concat(s,$"\t[{name}] {value}\n");
+		}
+		foreach (var (name,value) in _dj)
+		{
+			s = String.Concat(s,$"\t[{name}] {value.ToString()} "+"\t}\n");
+		}
+		return s;
+	}
 	public void print()
 	{
-		for(int i=0;i<_da.Count;i++)
-		{
-			Console.WriteLine(_da.ElementAt(i));
-		}
+		Console.WriteLine($"{ToString()}"+"}");
 	}
 	public string getString(string name)
 	{
-		string ls = "";
-		if(_da.ContainsKey(name)) ls = _da[name];
-		return ls;
+		string s = "";
+		if(_ds.ContainsKey(name)) s = _ds[name];
+		return s;
 	}
 	public int getInt(string name)
 	{
 		int o = -1;
-		Console.WriteLine("getInt "+name);
-		string v = getString(name);
-		if(v!=null)
-			o = Int32.Parse(v);
+		if(_di.ContainsKey(name)) o = _di[name];
 		return o;
 	}
 	public float getFloat(string name)
 	{
-		double o = -1;
-		string v = getString(name);
-		if(v!=null && v.Contains('.'))
-		{
-			for(int i=0;i<v.Length;i++)
-			{
-				if(v.ElementAt(i)=='.')
-				{
-					o = (float)Int32.Parse(v.Substring(0,i));
-					string ls = v.Substring(i+1,v.Length-(i+1));
-					o += Int32.Parse(ls)/Math.Pow(10,ls.Length);
-					break;
-				}
-			}
-			
-			//o = float.Parse(v);
-		}
-		return (float)o;
+		float o = -1;
+		if(_df.ContainsKey(name)) o = _df[name];
+		return o;
+	}
+	public JsonObject getObject(string name)
+	{
+		if(_dj.ContainsKey(name)) return _dj[name];
+		return new JsonObject("");
 	}
 	
 }
